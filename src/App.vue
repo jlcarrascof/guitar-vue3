86dd85b74929e6214672222ec3b9911927a07285
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-11-01 19:53:17 -0400

    feat: initialize reactive state for guitarras in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup>
+    import { ref, reactive } from 'vue';
 
+    const state = reactive({
+        guitarras: []
+    });
+
+    console.log(state.guitarras);
 </script>
 
 <template>
